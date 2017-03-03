31c188186e64cdfb7091c508f12eede9ef57643d
letulip <ivladimirskiy@ya.ru>
2017-03-03 15:04:32 +0300

    Queen and worker classes added

---
+++ BeeHiveManagementSystem/Queen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeHiveManagementSystem
+{
+    class Queen
+    {
+        private Worker[] workers;
+        private int shiftNumber;
+
+        public Queen(Worker[] workers)
+        {
+            this.workers = workers;
+        }
+
+        public bool AssignWork(string jobType, int shifts)
+        {
+            return true;
+        }
+
+        public void WorkNextSHift()
+        {
+
+        }
+    }
+}
---
+++ BeeHiveManagementSystem/Worker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeHiveManagementSystem
+{
+    class Worker
+    {
+        public string CurrentJob { get; }
+        public int ShiftsLeft { get; }
+
+        private string[] jobsICanDo;
+        private int shiftsToWork;
+        private int shiftsWorked;
+
+        public Worker(string[] jobsICanDo)
+        {
+            this.jobsICanDo = jobsICanDo;
+        }
+
+        public bool DoThisJob(bool IsFree)
+        {
+            if (IsFree)
+                return true;
+            else
+                return false;
+        }
+
+        public bool DidYouFinish(bool IsDone)
+        {
+            if (IsDone)
+                return true;
+            else
+                return false;
+        }
+    }
+}
